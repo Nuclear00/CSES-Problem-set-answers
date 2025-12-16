7584739655ad95205c5dcc8a16c5e475d615f24b
Nuke/Objektt <190471520+Nuclear00@users.noreply.github.com>
2025-12-16 22:02:45 +0100

    Add files via upload

---
+++ weird-algorithm/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int n; // change here for testing
+
+int odd() {
+	if(n % 2 == 1) {
+		n = n * 3 + 1;
+		printf("%d ", n);
+		return n;
+	} else {
+		n = n / 2;
+		printf("%d ", n);
+		return n;
+	}
+}
+
+int main() {
+	scanf("%d", &n);
+	while(n != 1) {
+		odd();
+	}
+	return 0;
+}
